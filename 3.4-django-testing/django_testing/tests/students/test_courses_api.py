26267b7b499053b55b89b5d42afb2eec1aa8f6d4
sergryap <rs1180@mail.ru>
2022-09-02 20:56:21 +0500

    HomeWork tests. Update commit

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -12,8 +12,13 @@ def client():
 
 
 @pytest.fixture
-def course():
-    return Course.objects.create(name='Test')
+def courses(course_factory):
+    return course_factory(_quantity=10)
+
+
+@pytest.fixture
+def id_course(courses):
+    return random.choice([course.pk for course in courses])
 
 
 @pytest.fixture
@@ -45,9 +50,7 @@ def test_get_courses(client, course_factory):
 
 # проверка фильтрации списка курсов по id
 @pytest.mark.django_db
-def test_filter_courses(client, course_factory):
-    courses = course_factory(_quantity=10)
-    id_course = courses[random.randint(0, len(courses) - 1)].pk
+def test_filter_courses(client, course_factory, id_course):
     response = client.get(f"/api/v1/courses/{id_course}/")
     data = response.json()
     assert data['id'] == id_course
@@ -55,9 +58,36 @@ def test_filter_courses(client, course_factory):
 
 # проверка фильтрации списка курсов по name
 @pytest.mark.django_db
-def test_filter_name_courses(client, course_factory):
-    courses = course_factory(_quantity=10)
+def test_filter_name_course(client, course_factory, courses):
     name_course = courses[random.randint(0, len(courses) - 1)].name
     response = client.get(f"/api/v1/courses/?name={name_course}")
     data = response.json()
     assert data[0]['name'] == name_course
+
+
+# тест успешного создания курса
+@pytest.mark.django_db
+def test_create_course(client):
+    count = Course.objects.count()
+    name_course = 'test_course'
+    client.post("/api/v1/courses/", data={'name': f'{name_course}'})
+    assert Course.objects.count() == count + 1
+    response = client.get(f"/api/v1/courses/?name={name_course}")
+    data = response.json()
+    assert data[0]['name'] == name_course
+
+
+# тест успешного обновления курса
+@pytest.mark.django_db
+def test_update_course(client, course_factory, id_course):
+    new_name_course = 'test_course'
+    data_update = {'name': f'{new_name_course}'}
+    client.patch(f"/api/v1/courses/{id_course}/", data=data_update)
+    assert Course.objects.get(pk=id_course).name == new_name_course
+
+
+# тест успешного удаления курса
+@pytest.mark.django_db
+def test_delete_course(client, course_factory, id_course):
+    client.delete(f"/api/v1/courses/{id_course}/")
+    assert Course.objects.filter(pk=id_course).count() == 0
