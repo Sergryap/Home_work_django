f6f7f4f80f81be8360768649c6bc047a22f2c819
sergryap <rs1180@mail.ru>
2022-09-02 23:20:36 +0500

    HomeWork tests. Update commit

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -8,3 +8,8 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate(self, data):
+        if len(data['students']) > 20:
+            raise serializers.ValidationError("Число студентов на курсе не должно быть больше 20!")
+        return data
